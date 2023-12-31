9f231a5fbe5350b9463478859ec05ab5ca7d326e
בתשבע אייזנבך <beisenba@g.jct.ac.il>
2023-12-31 15:11:44 +0200

    Initial code+Welcome function

---
+++ Stage0/Program3605.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Stage0
+{
+     partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome3605();
+            Welcome9370();
+
+            Console.ReadKey();
+        }
+        static partial void Welcome9370();
+
+        private static void Welcome3605()
+        {
+            Console.WriteLine("Enter your name: ");
+            string? name = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first console application", name);
+        }
+    }
+}
