00d3df7dc7f1ef2f65958d85a99cc9e72838d7ad
Esti Walles <ewalles@g.jct.ac.il>
2024-01-06 21:57:59 +0200

    creating interface to task and dependency

---
+++ DalFacade/DalApi/IDependency.cs
@@ -0,0 +1,13 @@
+﻿
+namespace DalApi;
+using DO;
+
+public interface IDependency
+{
+    int Create(Dependency item); //Creates new entity of Dependency in DAL
+    Dependency? Read(int id); //Reads entity of Dependency by its ID 
+    List<Dependency> ReadAll(); //stage 1 only, Reads all entity of Dependencies
+    void Update(Dependency item); //Updates entity of Dependency
+    void Delete(int id); //Deletes an object of Dependency by its Id
+
+}
---
+++ DalFacade/DalApi/ITask.cs
@@ -0,0 +1,12 @@
+﻿
+namespace DalApi;
+using DO;
+public interface InTask
+{
+    int Create(Task item); //Creates new entity of task in DAL
+    Task? Read(int id); //Reads entity of task by its ID 
+    List<Task> ReadAll(); //stage 1 only, Reads all entity of tasks
+    void Update(Task item); //Updates entity of task
+    void Delete(int id); //Deletes a task by its Id
+
+}
