03d110f18f9aa6573a846eed9669e533c26d0bf9
Esti Walles <ewalles@g.jct.ac.il>
2024-01-07 18:52:42 +0200

    stage 8 c for task

--- DalFacade/DO/Task.cs
+++ DalFacade/DO/Task.cs
@@ -8,11 +8,11 @@
 /// <param name="Name">The name of the task</param>
 /// <param name="Descriptoin">Describes the goal of the task</param>
 /// <param name="Id">Uniqe id that helps to recognize the task</param>
-/// <param name="TimeOfTask">A date that the engineer started the task</param>
+/// <param name="RiquiredEffortTime">The length of time that the task takes</param>
 /// <param name="Product">Describes the final result of the task</param>
 /// <param name="Complexity">The minimum level of engineer that can do the task</param>
 /// <parm  name="Engineerid">The level of the engineer that was assigned to the task</parm> 
-/// <param name="MileStone">In the Dal layer this field will be false and in the next levels it would be able to be changed to true</param>
+/// <param name="IsMileStone">In the Dal layer this field will be false and in the next levels it would be able to be changed to true</param>
 /// <param name="OptionalDeadline">Optional maximum final date to the task</param>
 /// <param name="CreateDate">The date of the creation of the task by the manager</param>
 /// <param name="StartDate">The planned date to start the task</param>
@@ -25,10 +25,10 @@ public record Task
     string? Name,
     string? Descriptoin,
     int Id,
-    int TimeOfTask,
     string Product,
     EngineerLevel Complexity,
     int Engineerid,
+    TimeSpan RiquiredEffortTime=new TimeSpan(),
     bool? IsMileStone = false,
     DateTime? OptionalDeadline = null,
     DateTime? CreateDate = null,
@@ -42,7 +42,7 @@ public record Task
     /// <summary>
     /// This is an empty ctor
     /// </summary>
-    public Task() : this("", "", 0, 0, "", 0, 0) { }
+    public Task() : this("", "", 0, "", 0, 0) { }
 }
    //We chose to write the record in the second way which the parameters ctor is already exists
    
--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -1,6 +1,10 @@
 ﻿namespace DalTest;
 using DalApi;
 using DO;
+using Microsoft.VisualBasic;
+using System.Data;
+using System.Security.Cryptography;
+using System.Text;
 
 public static class Initialization
 {
@@ -8,5 +12,46 @@ public static class Initialization
     private static IEngineer? s_dalEngineer; //stage 1
     private static IDependency? s_dalDependency; //stage 1
     private static readonly Random s_rand = new();//The entities will use this random in order to fill the objects values
+    private static void createTask()
+    {
+        string[] tasksNames =
+            {"Analysis","Planning","Design","Prototyping","Testing","Simulation","Integration",
+            "Coding","Evaluation","Estimation","Risk","Quality","Compliance","Management","Scheduling",
+            "Procurement","Communication","Documentation","Safety","Commissioning"};
+        string[] tasksDescription =
+            {" Assess project feasibility, risks, and requirements","Define project goals, scope, and objectives.",
+            "Develop conceptual and detailed plans for project implementation","Build initial models or prototypes for testing and validation.",
+            " Conduct comprehensive tests to ensure functionality and reliability.","Use simulations to model and analyze system behavior",
+            "Combine individual components into a unified system.","Write and implement software or programming code.",
+            "Assess project performance against predefined criteria"," Estimate project costs, resources, and timelines.",
+            "Identify, analyze, and mitigate potential project risks.","Implement measures to ensure high standards of product or service.",
+            "Ensure adherence to relevant regulations and standards.","Oversee project activities, resources, and team coordination.",
+            "Develop and manage project timelines and milestones.","Acquire necessary materials, equipment, and services.",
+            " Facilitate effective information exchange within the team.","Create and maintain project-related documentation.",
+            " Implement measures to ensure a safe working environment."," Initiate and oversee the deployment of the project."};
+        string[] tasksProduct =
+        {
 
+            "Detailed report assessing project feasibility, risks, and requirements","Comprehensive project plan outlining goals, scope, and objectives",
+            "Comprehensive design plans for project implementation.",
+            " Initial working model or prototype for testing and validation","Documented results of comprehensive tests ensuring functionality.",
+            "Detailed model illustrating system behavior under different conditions.","Fully assembled and functional system.",
+            "Software or programming code implemented and ready for use.","Assessment of project performance against predefined criteria.",
+            "Detailed estimate of project costs, resources, and timelines","Documented strategies to identify, analyze, and mitigate potential risks.",
+            "Implementation measures ensuring high standards of the final product.","Documented proof of adherence to relevant regulations and standards.",
+            "Overview of project activities, resources, and coordination."," Detailed project timeline with milestones and deadlines",
+            "Necessary materials, equipment, and services procured for the project.","Documented plan for effective information exchange within the team.",
+            "Comprehensive and organized documentation related to the project.","Documented implementation of safety measures in the project.","Successfully deployed and fully operational project."
+        };
+        foreach (var _name in tasksNames) foreach(var _description in tasksDescription) foreach (var _product in tasksProduct)
+                {
+                int effortTime = s_rand.Next(30,180);
+                EngineerLevel complex= (EngineerLevel)s_rand.Next((int)EngineerLevel.Beginner, (int)EngineerLevel.Expert);
+                int engineerid = s_rand.Next(100000000, 999999999);
+                Task newTask = new(_name, _description,0,_product,complex,engineerid, TimeSpan.FromDays(effortTime) ,false,null,null,null,null,null,null);
+
+            s_dalTask!.Create(newTask);
+        }
+
+    }
 }
