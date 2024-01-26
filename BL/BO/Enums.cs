714e864a1c3ab8544bd742fa15272278e2010745
Esti Walles <ewalles@g.jct.ac.il>
2024-01-26 09:10:29 +0200

    Enums class for BO

---
+++ BL/BO/Enums.cs
@@ -0,0 +1,20 @@
+﻿namespace BO;
+public enum EngineerLevel
+{
+    Beginner,
+    AdvancedBeginner,
+    Intermediate,
+    Advanced,
+    Expert
+}
+public enum Status
+{
+    Unschedeked,
+    Schedeked,
+    OnTrack,
+    InJeopardy,
+    Done
+}
+
+
+
