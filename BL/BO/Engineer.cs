dd57c16c430db48c9d84d0fe9a9f81f9f94afe3a
Esti Walles <ewalles@g.jct.ac.il>
2024-01-26 09:36:16 +0200

    Adding classes in BO

---
+++ BL/BO/Engineer.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace BO;
+
+public class Engineer
+{
+    int Id { get; init; }
+    string Name { get; set; }  
+    string Email { get; set; }
+    EngineerLevel Level { get; set; }
+    double Cost { get; set; }
+    BO.TaskInEngineer? Task { get; set; }
+}
