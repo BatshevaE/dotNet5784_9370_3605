c2eb6c4e4a367b3a8ef52c026371b8f6329c4bcf
בתשבע אייזנבך <beisenba@g.jct.ac.il>
2024-01-28 13:25:47 +0200

    level 9

---
+++ BL/BlImplementation/Bl.cs
@@ -0,0 +1,12 @@
+﻿namespace BlImplementation;
+using BlApi;
+internal class Bl : IBl
+{
+    public ITask Task =>  new TaskImplemenation();
+
+    public IEngineer Engineer =>  new EngineerImplemented();
+
+    public ITaskInList TaskInList =>  new TaskInListImplemented();
+
+    public IEngineerInTask EngineerInTask =>  new EngineerInTaskImplemented();
+}
